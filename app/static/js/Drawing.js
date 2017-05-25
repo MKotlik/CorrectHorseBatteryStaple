49620cd3315ba3999285256c4207812bdf9b2494
Kenneth Li <kennethli.3470@gmail.com>
2017-05-25 12:13:55 -0400

    test

--- app/static/js/Drawing.js
+++ app/static/js/Drawing.js
@@ -6,16 +6,25 @@ function Drawing(width, height, scene) {
 
 Drawing.create = function (width, height, scene) {
     return new Drawing(width, height, scene);
-}
+};
 
-Drawing.prototype.renderBox = function (x, y, z, l, w, h, color) {
-    var geometry = new THREE.BoxGeometry(l, w, h);
+Drawing.prototype.renderGrain = function (grain) {
+    this.renderBox(grain.position, grain.dimensions, grain.color);
+};
+
+Drawing.prototype.renderBox = function (position, dimensions, color) {
+    var geometry = new THREE.BoxGeometry(dimensions.x,
+                                         dimensions.y,
+                                         dimensions.z);
     
     var material = new THREE.MeshBasicMaterial({
         color: color
     });
-
+    
     var mesh = new THREE.Mesh(geometry, material);
+
+    mesh.position.copy(position);
+    
     this.scene.add(mesh);
     return mesh;
 };
