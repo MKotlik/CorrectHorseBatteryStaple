57bacaafb9ca66d9703ed2f3810978989572b077
nckng <nng3@stuy.edu>
2017-06-01 12:29:52 -0400

    started listing

--- app/app.py
+++ app/app.py
@@ -43,7 +43,7 @@ def root():
 def home():
     '''Displays the homepage, diff versions based on login status'''
     if is_logged_in():
-        return render_template('home_user.html')
+        return render_template('home_user.html',owned_projects=display_projects(session['username']))
     else:
         return render_template('home_public.html')
 
@@ -191,6 +191,14 @@ def get_username():
 def is_password_valid(password):
     # Currently password length is only req, but can expand
     return len(password) >= 8
+
+# ===== DISPLAY HELPERS ===== #
+def display_projects(username):
+    own_projects = db_users.get_owned_projects(username)
+    retstr = ''
+    for project in own_projects[1]:
+        retstr += '<a href="/project/'+ str(project['projID'])+'" class="list-group-item">'+ project['name'] +'</a>\n'
+    return retstr
 
 
 # -- run module -- #
